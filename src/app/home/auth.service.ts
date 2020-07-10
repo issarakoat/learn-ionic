5d9370123675a0bd0042796ee3118a9c22048348
issarakoat <weeravut.issarakoat@gmail.com>
2020-07-09 23:56:47 -0600

    just make it functional better

--- src/app/home/auth.service.ts
+++ src/app/home/auth.service.ts
@@ -1,41 +1,44 @@
-import { Injectable } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/auth";
-import { auth } from "firebase/app";
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 import AuthProvider = firebase.auth.AuthProvider;
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class AuthService {
   constructor(public afAuth: AngularFireAuth) {}
-  //this login would work too
-  loginWithGoogle() {
-    return <toPromise>this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
+  // this login would work too
+  loginWithGoogle(): Promise<object> {
+    return this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
   }
   signInWithGoogle() {
-    console.log("Sign in with Google");
-    return this.oauthSignIn(new auth.GoogleAuthProvider());
-  }
-  loggoutWithGoogle(){
-     return this.afAuth.signOut();
-  }
-
-  private oauthSignIn(provider: AuthProvider) {
-    if (!(<any>window).cordova) {
-      return this.afAuth.signInWithPopup(provider);
-    } else {
-      return this.afAuth.signInWithRedirect(provider).then(() => {
-        return this.afAuth
-          .getRedirectResult()
-          .then((result) => {
-            let user = result.user;
-            console.log(user);
-          })
-          .catch(function (error) {
-            // Handle Errors here.
-            alert(error.message);
-          });
-      });
+    console.log('Sign in with Google');
+    // return this.oauthSignIn(new auth.GoogleAuthProvider());
+    if (!(window as any).cordova) {
+      return this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
     }
   }
+  loggoutWithGoogle() {
+    return this.afAuth.signOut();
+  }
+  //// I dont think i need the thing down here
+  // private oauthSignIn(provider: AuthProvider) {
+  //   if (!(window as any).cordova) {
+  //     return this.afAuth.signInWithPopup(provider);
+  //   } else {
+  //     return this.afAuth.signInWithRedirect(provider).then(() => {
+  //       return this.afAuth
+  //         .getRedirectResult()
+  //         .then((result) => {
+  //           const user = result.user;
+  //           console.log(user);
+  //         })
+  //         .catch( (error) => {
+  //           // Handle Errors here.
+  //           alert(error.message);
+  //         });
+  //     });
+  //   }
+  // }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,28 +12,21 @@ export class HomePage implements OnInit {
   userEmail = '';
   userImage = '';
   userID = '';
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-  }
-  onGoogleLoggin(){
-    this.authService.signInWithGoogle().then(data => {
-      console.log(data.user);
-      console.log(data.user.displayName);
-      console.log(data.user.email);
-      console.log(data.user.uid);
-      console.log(data.user.photoURL);
+  ngOnInit() {}
+  onGoogleLoggin() {
+    this.authService.signInWithGoogle().then((data) => {
       this.userEmail = data.user.email;
       this.userName = data.user.displayName;
       this.userImage = data.user.photoURL;
       this.userID = data.user.uid;
       this.isLoggin = true;
-    })
+    });
   }
-  onGoogleLoggout(){
-    this.authService.loggoutWithGoogle().then( ()=> {
+  onGoogleLoggout() {
+    this.authService.loggoutWithGoogle().then(() => {
       this.isLoggin = false;
-    })
+    });
   }
-
 }
